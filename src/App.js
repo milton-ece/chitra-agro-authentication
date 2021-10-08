3e3457c626d28efd55ab111f9299ca8a44095bbb
milton-ece <milton97@gmail.com>
2021-10-08 18:31:02 +0600

    toggle done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { getAuth, signOut, signInWithPopup, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
+import { getAuth, signOut, signInWithPopup, GoogleAuthProvider, GithubAuthProvider, createUserWithEmailAndPassword } from "firebase/auth";
 import logo from './logo.svg';
 import './App.css';
 import initializeAuthentication from './firebase/firebase.initialize';
@@ -10,8 +10,12 @@ const googleProvider = new GoogleAuthProvider();
 const gitHubprovider = new GithubAuthProvider();
 
 function App() {
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState({});
   const auth = getAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [isLogin, setIsLogin] = useState(false);
 
   const handleGoogleSignIn = () => {
 
@@ -48,22 +52,71 @@ function App() {
       })
   }
 
+  const toggleLogin = e => {
+    // console.log(e.target.checked)
+    setIsLogin(e.target.checked)
+  }
+
+  const handleEmailChange = e => {
+    // console.log(e.target.value);
+    setEmail(e.target.value);
+  }
+  const handlePasswordChange = e => {
+    // console.log(e.target.value);
+    setPassword(e.target.value);
+  }
   const handleRegistration = e => {
-    console.log('registration will be added');
     e.preventDefault();
+    console.log(email, password);
+    if (password.length < 6) {
+      setError('Password must be at least 6 characters long')
+      return;
+    }
+    if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+      setError('Password must contain 2 upper case');
+      return;
+    }
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+        setError('');
+      })
+      .catch(error => {
+        setError(error.message);
+      })
+
   }
 
   return (
-    <div className="App">
+    <div className="mx-5">
       <form onSubmit={handleRegistration}>
-        <h3>Please Register</h3>
-        <label htmlFor="email">Email:</label>
-        <input type="text" name="email" />
-        <br />
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="" />
-        <br />
-        <input type="submit" value="Register" />
+        <h3 className="text-primary">Please {isLogin ? 'Login' : 'Register'}</h3>
+        <div className="row mb-3">
+          <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
+          <div className="col-sm-10">
+            <input onBlur={handleEmailChange} type="email" className="form-control" id="inputEmail3" required />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
+          <div className="col-sm-10">
+            <input type="password" onBlur={handlePasswordChange} className="form-control" id="inputPassword3" required />
+          </div>
+        </div>
+
+        <div className="row mb-3">
+          <div className="col-sm-10 offset-sm-2">
+            <div className="form-check">
+              <input onChange={toggleLogin} className="form-check-input" type="checkbox" id="gridCheck1" />
+              <label className="form-check-label" htmlFor="gridCheck1">
+                Already Registered?
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="row mb-3 text-danger">{error}</div>
+        <button type="submit" className="btn btn-primary"> {isLogin ? 'Login' : 'Register'}</button>
       </form>
       <br /><br /><br />
 
