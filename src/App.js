4ba8122b8cae2467066e406f7061bce77da5d355
milton-ece <milton97@gmail.com>
2021-10-08 06:45:40 +0600

    register partial done

--- src/App.js
+++ src/App.js
@@ -47,8 +47,28 @@ function App() {
         setUser({});
       })
   }
+
+  const handleRegistration = e => {
+    console.log('registration will be added');
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
+      <form onSubmit={handleRegistration}>
+        <h3>Please Register</h3>
+        <label htmlFor="email">Email:</label>
+        <input type="text" name="email" />
+        <br />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="" />
+        <br />
+        <input type="submit" value="Register" />
+      </form>
+      <br /><br /><br />
+
+      <div>---------------------------------</div>
+      <br /><br /><br />
       {!user.name ?
         <div>
           <button onClick={handleGoogleSignIn}>Google Sign In</button>
